d8bc6088d7aaf2050be2e6b2442efa0aa95f1ef9
Nielzon66 <128815918+Nielzon66@users.noreply.github.com>
2025-04-01 11:19:56 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+function generatePassword() {
+    const length = 12;
+    const charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+";
+    let password = "";
+    for (let i = 0; i < length; i++) {
+        password += charset.charAt(Math.floor(Math.random() * charset.length));
+    }
+    document.getElementById("password").value = password;
+}
+
+function copyPassword() {
+    const passwordField = document.getElementById("password");
+    passwordField.select();
+    document.execCommand("copy");
+    alert("Senha copiada!");
+}
